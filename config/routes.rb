cd85f16db59910ed3f0546e0b83bdd27bf6a2409
tianshuai <157058166@qq.com>
2016-12-25 12:55:15 +0800

    join & quit group action

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :groups do
-       member do
-         post :join
-         post :quit
-       end
+   member do
+     post :join
+     post :quit
+   end
 
-    resources :posts
-  end
+  resources :posts
+end
   
   root 'groups#index'
 end
